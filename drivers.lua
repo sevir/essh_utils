b75cb1e8723f725b65971f81da2e4992d44e7759
Jose F. Rives Lirola <jose@digio.es>
2025-03-05 16:43:45 +0100

    fix(drivers): remove tabs

--- drivers.lua
+++ drivers.lua
@@ -1,45 +1,47 @@
 driver "deno" { 
     engine = [=[
-        {{template "environment" .}}
+{{template "environment" .}}
 
-        # Detect if deno is installed
-        if ! command -v deno &> /dev/null
-        then
-            echo "Deno could not be found. Please install it."
-            exit 1
-        fi
-        
-        set -e
+# Detect if deno is installed
+if ! command -v deno &> /dev/null
+then
+    echo "Deno could not be found. Please install it."
+    exit 1
+fi
+
+set -e
+
 deno run --unstable -q -A - <<EOF
 {{range $i, $script := .Scripts -}}
     {{$script.code}} 
 {{end -}}
 
 EOF
-        set +e
+
+set +e
     ]=],
 }
 
 driver "python" {
     engine = [=[
-        {{template "environment" .}}
+{{template "environment" .}}
 
-        # Detect if python or python3 is installed, python3 is preferred
-        if ! command -v python3 &> /dev/null
-        then
-            if ! command -v python &> /dev/null
-            then
-                echo "Python could not be found. Please install it."
-                exit 1
-            else
-                CMD=python
-            fi
-        else
-                CMD=python3
-        fi
-        
-        
-        set -e
+# Detect if python or python3 is installed, python3 is preferred
+if ! command -v python3 &> /dev/null
+then
+    if ! command -v python &> /dev/null
+    then
+        echo "Python could not be found. Please install it."
+        exit 1
+    else
+        CMD=python
+    fi
+else
+        CMD=python3
+fi
+
+
+set -e
 
 $CMD <<EOF
 {{range $i, $script := .Scripts -}}
@@ -47,27 +49,27 @@ $CMD <<EOF
 {{end -}}
 EOF
 
-        set +e
+set +e
     ]=],
 }
 
 driver "essh-lua" {
     engine = [=[
-        {{template "environment" .}}
+{{template "environment" .}}
 
-        # Detect if essh is installed
-        if ! command -v essh &> /dev/null
-        then
-            # Try to extend the PATH
-            export PATH=$PATH:$HOME/.bin
-            if ! command -v essh &> /dev/null
-            then
-                echo "essh could not be found. Please install it."
-                exit 1
-            fi
-        fi
-        
-        set -e
+# Detect if essh is installed
+if ! command -v essh &> /dev/null
+then
+    # Try to extend the PATH
+    export PATH=$PATH:$HOME/.bin
+    if ! command -v essh &> /dev/null
+    then
+        echo "essh could not be found. Please install it."
+        exit 1
+    fi
+fi
+
+set -e
 
 essh --eval <<EOF
 {{range $i, $script := .Scripts -}}
